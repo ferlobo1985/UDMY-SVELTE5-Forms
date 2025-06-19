e7f5fabdc8db957e548b095a9dfd780cb3b20a7f
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-19 13:22:01 -0300

    1st commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let name = $state('');
+  let age = $state(0);
+
+  $inspect(name,age)
+</script>
+
+<div class="container">
+  <h1>My form</h1>
+
+  <div class="mb-3">
+    <label for="name">Name</label>
+    <input type="text" bind:value={name} class="form-control"/>
+  </div>
+
+  <div class="mb-3">
+    <label for="age">Age</label>
+    <input type="number" bind:value={age} class="form-control"/>
+  </div>
+</div>
